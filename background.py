bd06fd5755a1b254a1d324823df713663252e320
Faunja <129631000+Faunja@users.noreply.github.com>
2025-02-22 22:05:12 -0500

    Add files via upload

---
+++ background.py
@@ -0,0 +1,61 @@
+# background.py
+# By Kayden Campbell
+# Copyright 2025
+# Licensed under the terms of the GPL 3
+import pygame
+from variables import *
+
+backHexagon = (0, 0, 0)
+frontHexagon = (210, 210, 210)
+
+def draw_hexagon(x, y, hexagon_radius):
+    straight_x = hexagon_radius * (4 / 5)
+    corner_x = straight_x / 2
+    corner_y = straight_x * (3 ** (1/2) / 2)
+    pygame.draw.polygon(screen, frontHexagon, ((x + corner_x, y + corner_y), (x + straight_x, y), (x + corner_x, y - corner_y), (x - corner_x, y - corner_y), (x - straight_x, y), (x - corner_x, y + corner_y)))
+
+def draw_background_hexagon(x, y, hexagon_radius):
+    straight_x = hexagon_radius
+    corner_x = straight_x / 2
+    corner_y = straight_x * (3 ** (1/2) / 2)
+    pygame.draw.polygon(screen, backHexagon, ((x + corner_x, y + corner_y), (x + straight_x, y), (x + corner_x, y - corner_y), (x - corner_x, y - corner_y), (x - straight_x, y), (x - corner_x, y + corner_y)))
+    draw_hexagon(x, y, hexagon_radius)
+
+def draw_background(cameraPosition):
+    center_x = round(SCREEN_WIDTH / 2) + cameraPosition[0]
+    center_y = round(SCREEN_HEIGHT / 2) + cameraPosition[1]
+
+    # Define the Radius of the hexagon
+    hexagon_radius = SCREEN_WIDTH / 32
+    # Calculates the distance between hexagons
+    x_distance = hexagon_radius * (3 / 2)
+    y_distance = hexagon_radius * (3 ** (1/2) / 2)
+    # Calculates the cutoff distance of hexagons
+    x_cutoff = [SCREEN_WIDTH + x_distance, -x_distance]
+    y_cutoff = [SCREEN_HEIGHT + y_distance, -y_distance]
+
+    x_offset = round(cameraPosition[0] / x_distance)
+    y_offset = round(cameraPosition[1] / y_distance)
+    if y_offset % 2 != 0:
+        y_offset += 1
+    x_repeat = round(SCREEN_WIDTH / x_distance)
+    y_repeat = round(SCREEN_HEIGHT / y_distance)
+    if y_repeat % 2 != 0:
+        y_repeat += 1
+    
+    screen.fill(White)
+    for x in range(-x_repeat - x_offset, x_repeat + 1 - x_offset):
+        x_position = center_x + x_distance * x
+        if x_position > x_cutoff[0] or x_position < x_cutoff[1]:
+            continue
+        for y in range(-y_repeat - 2 - y_offset, y_repeat + 1 - y_offset, 2):
+            if x % 2 == 0:
+                y_position = center_y + y_distance * y
+            else:
+                y_position = center_y + y_distance * (y + 1)
+            if y_position > y_cutoff[0] or y_position < y_cutoff[1]:
+                continue
+            draw_background_hexagon(x_position, y_position, hexagon_radius)
+
+    pygame.display.set_caption(f'{clock.get_fps() :.1f}')
+    pygame.display.update()
---
+++ main.py
@@ -0,0 +1,67 @@
+# main.py
+# By Kayden Campbell
+# Copyright 2025
+# Licensed under the terms of the GPL 3
+# If pygame is not installed: sudo apt install python3-pygame
+# To run: python3 Racing-Game/main.py
+import pygame
+from variables import *
+from background import draw_background
+
+def main():
+	run = True
+	screenGrab = False
+	newmousePosition = (0, 0)
+	oldmousePosition = (0, 0)
+	
+	cameraPosition = [0, 0]
+	cameraVelocity = [0, 0]
+	cameraFriction = .9
+	centerCamera = False
+	centerSpeed = .9
+
+	while run:
+		clock.tick(FPS)
+		for event in pygame.event.get():
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 1:
+					screenGrab = True
+					centerCamera = False
+			if event.type == pygame.MOUSEBUTTONUP:
+				if event.button == 1:
+					if screenGrab == True:
+						screenGrab = False
+						cameraVelocity[0] += newmousePosition[0] - oldmousePosition[0]
+						cameraVelocity[1] += newmousePosition[1] - oldmousePosition[1]
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					run = False
+				if event.key == pygame.K_c:
+					if screenGrab == False:
+						centerCamera = True
+			if event.type == pygame.QUIT:
+				run = False
+		
+		oldmousePosition = newmousePosition
+		newmousePosition = pygame.mouse.get_pos()
+		if newmousePosition[0] > 0 and newmousePosition[0] < SCREEN_WIDTH and newmousePosition[1] > 0 and newmousePosition[1] < SCREEN_HEIGHT:
+			if screenGrab == True:
+				cameraPosition[0] += newmousePosition[0] - oldmousePosition[0]
+				cameraPosition[1] += newmousePosition[1] - oldmousePosition[1]
+		else:
+			if screenGrab == True:
+				screenGrab = False
+				cameraVelocity[0] += newmousePosition[0] - oldmousePosition[0]
+				cameraVelocity[1] += newmousePosition[1] - oldmousePosition[1]
+		if centerCamera == False:
+			if screenGrab == False:
+				cameraPosition[0] += cameraVelocity[0]
+				cameraPosition[1] += cameraVelocity[1]
+				cameraVelocity[0] *= cameraFriction
+				cameraVelocity[1] *= cameraFriction
+		else:
+			cameraPosition[0] *= centerSpeed
+			cameraPosition[1] *= centerSpeed
+		draw_background(cameraPosition)
+	pygame.quit()
+main()
